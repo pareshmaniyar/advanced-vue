58d85a5f401763eda03b5e4989645a3a86c578c8
pareshmaniyar <pareshmaniyar1@gmail.com>
2020-05-07 20:25:07 +0530

    Implement scss; remove package-lock;

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div>
+        <div class="title">
             Wow! That's fun!
         </div>
         <header>
@@ -17,5 +17,19 @@ export default {
     name: 'app'
 }
 </script>
-<style>
+<style lang="scss" scoped>
+    @import 'App.scss';
+    header {
+        a {
+            background-color: #4CAF50; /* Green */
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 10px;
+        }
+    }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,14 @@ module.exports = {
                 use: {
                     loader: 'vue-loader'
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
