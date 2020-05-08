824ec8cf84e4aa1a9360551423a86dceba58ef94
pareshmaniyar <pareshmaniyar1@gmail.com>
2020-05-08 19:40:08 +0530

    Partial implementation of Vue module

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,53 @@
                 </b-navbar-nav>
             </b-navbar>
         </div>
-        <b-card :title="title">
-            Practice makes a man confident in the Interview
+        <b-card>
+        <h2>{{title}}</h2>
+        <h6><i>"Practice makes a man confident in the Interview"</i></h6>
+        <b-card-group deck>
+            <b-card
+                border-variant="secondary"
+                header="Features"
+                header-border-variant="secondary"
+                align="center"
+            >
+                <b-card-text>
+                    <b-form>
+                        <b-form-group id="input-group-2" label="Add features:" label-for="input-2">
+                            <b-form-input
+                            id="input-2"
+                            v-model="feature_to_add"
+                            required
+                            placeholder="Enter Features"
+                            v-on:keyup.enter="onFeatureSubmit"
+                            ></b-form-input>
+                        </b-form-group>
+                    </b-form>
+                    <b-list-group>
+                        <b-list-group-item v-for="(item, index) in features">
+                            {{item}}
+                            <b-button @click="removeFeature(index)">Remove</b-button>
+                        </b-list-group-item>
+                    </b-list-group>
+                </b-card-text>
+            </b-card>
+            <b-card
+                border-variant="secondary"
+                header="Secondary"
+                header-border-variant="secondary"
+                align="center"
+            >
+                <b-card-text>
+                    <b-list-group>
+                        <b-list-group-item>Cras justo odio</b-list-group-item>
+                        <b-list-group-item>Dapibus ac facilisis in</b-list-group-item>
+                        <b-list-group-item>Morbi leo risus</b-list-group-item>
+                        <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
+                        <b-list-group-item>Vestibulum at eros</b-list-group-item>
+                    </b-list-group>
+                </b-card-text>
+            </b-card>
+        </b-card-group>
         </b-card>
         <router-view v-on:changetitle="changeTitle($event)">
             <b>Random stuff to see in the footer</b>
@@ -26,12 +71,15 @@ export default {
     name: 'app',
     data() {
         return {
-            title: 'Wow! That\'s fun!'
+            title: 'Wow! That\'s fun!',
+            feature_to_add: '',
+            features: ['asd','asfdf']
         }
     },
     computed: {
         ...mapGetters({
-            lightMode: 'mode'
+            lightMode: 'mode',
+            features
         })
     },
     created(){
@@ -40,6 +88,13 @@ export default {
     methods: {
         changeTitle(title) {
             this.title = title;
+        },
+        onFeatureSubmit(){
+            console.log("onFeatureSubmit");
+            this.feature_to_add = '';
+        },
+        removeFeature(index){
+            console.log("onFeatureSubmit");
         }
     }
 }
--- src/mixin.js
+++ src/mixin.js
@@ -1,9 +1,7 @@
 import { eventBus } from './index'
 
 export const firtMixin = {
-    created() {
-        console.log("Using mixin");
-    },
+    
     methods: {
         changeTitle(){
             eventBus.$emit('ChangeTitle', 'Wow! That Blog was fun!');
